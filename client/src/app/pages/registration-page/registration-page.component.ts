39a8ec9bcded71c68d74c44f1ffa13e305308936
DmiKul <mr.crocodile567@gmail.com>
2023-06-22 13:40:07 +0300

    feat: add form to component, replace tag a with button

--- client/src/app/pages/registration-page/registration-page.component.ts
+++ client/src/app/pages/registration-page/registration-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from 'src/app/api/services/login.service';
 @Component({
@@ -8,15 +9,31 @@ import { LoginService } from 'src/app/api/services/login.service';
   styleUrls: ['./registration-page.component.less']
 })
 export class RegistrationPageComponent {
-  testForm = new FormGroup({
-    testValue: new FormControl('mail@mail.ru'),
+  // testForm = new FormGroup({
+  //   testValue: new FormControl('mail@mail.ru'),
+  // });
+
+  form = this.formBuilder.group({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    phoneNumber: new FormControl(''),
+    email: new FormControl(''),
+    gender: new FormControl('')
   });
 
-  constructor(private router: Router, private loginService: LoginService) {
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private formBuilder: FormBuilder
+  ) {
     this.loginService.setIsLoggedIn(false);
   }
 
-  backToLogin() {
-    this.router.navigate(['/login'])
+  backToLogin(): void {
+    this.router.navigate(['/login']);
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value);
   }
 }
