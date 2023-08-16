df6c0798e8a8989d142b48bfa12536a388299f2d
DmiKul <mr.crocodile567@gmail.com>
2023-08-16 17:52:39 +0300

    feat: add changing like and dislike logic of post

--- src/app/shared/modules/post/components/post-control-bar/post-control-bar.component.ts
+++ src/app/shared/modules/post/components/post-control-bar/post-control-bar.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-control-bar.component.less']
 })
 export class PostControlBarComponent {
+  isLiked: boolean = false
+  isDisliked: boolean = false
 
+  constructor() {
+    //todo: устанавливать значения лайка, дизлайка
+  }
+
+  like() {
+    if (!this.isLiked) {
+      this.isDisliked = false
+    }
+    this.isLiked = !this.isLiked
+  }
+
+  dislike() {
+    if (!this.isDisliked) {
+      this.isLiked = false
+    }
+    this.isDisliked = !this.isDisliked
+  }
 }
