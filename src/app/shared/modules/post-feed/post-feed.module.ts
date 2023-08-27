e6e5528a4e73a0b56e3ecbb78c32850d5cab4d32
DmiKul <mr.crocodile567@gmail.com>
2023-08-27 13:43:41 +0300

    fix buttons display in post

--- src/app/shared/modules/post-feed/post-feed.module.ts
+++ src/app/shared/modules/post-feed/post-feed.module.ts
@@ -6,10 +6,11 @@ import { PostControlBarComponent } from './components/post-control-bar/post-cont
 import { PostImagesComponent } from './components/post-images/post-images.component';
 import { PostHeaderComponent } from './components/post-header/post-header.component';
 import { PostTextComponent } from './components/post-text/post-text.component';
-import { TuiDataListModule, TuiHostedDropdownModule, TuiLoaderModule, TuiSvgModule } from '@taiga-ui/core';
+import { TuiButtonModule, TuiDataListModule, TuiHostedDropdownModule, TuiLoaderModule, TuiSvgModule } from '@taiga-ui/core';
 import { SectionModule } from '../section/section.module';
 import { UserAvatarModule } from '../user-avatar/user-avatar.module';
 import { TuiCarouselModule, TuiPaginationModule } from '@taiga-ui/kit';
+
 
 @NgModule({
   declarations: [
@@ -30,7 +31,9 @@ import { TuiCarouselModule, TuiPaginationModule } from '@taiga-ui/kit';
     TuiCarouselModule,
     TuiSvgModule,
     TuiPaginationModule,
-    
+    TuiHostedDropdownModule,
+    TuiButtonModule
+
   ],
   exports: [PostFeedComponent]
 })
